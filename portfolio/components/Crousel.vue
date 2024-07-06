4a52d657fd1b5bf3de19eeff17977975bf67b821
Vipul Jain <vjain2376@gmail.com>
2024-07-07 01:22:42 +0530

    installed swiper and built a crousel out of it

---
+++ portfolio/components/Crousel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <Swiper
+      :modules="[SwiperAutoplay, SwiperNavigation, SwiperPagination]"
+      :slides-per-view="1"
+      :loop="true"
+      effect="creative"
+      :navigation="true"
+      :pagination="true"
+      :autoplay="{
+        delay: 8000,
+        disableOnInteraction: true,
+      }"
+    >
+      <SwiperSlide v-for="(slide, idx) in slides" :key="idx">
+        <img :src="`${slide.url}`" />
+      </SwiperSlide>
+    </Swiper>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  slides: {
+    type: Array,
+    required: true,
+  },
+});
+
+const { slides } = toRefs(props);
+</script>
+
+<style>
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  height: 20vh;
+  font-size: 4rem;
+  font-weight: bold;
+  font-family: "Roboto", sans-serif;
+}
+
+.swiper-wrapper {
+  width: 100%;
+}
+
+.swiper-cards {
+  width: 240px;
+  height: 240px;
+}
+
+.swiper-cards .swiper-slide {
+  border-radius: 6px;
+  border: 1px solid black;
+}
+</style>
--- portfolio/components/Projects.vue
+++ portfolio/components/Projects.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="max-h-full max-w-full bg-slate-300 text-slate-900 font-serif px-12 py-12">
+  <div
+    class="max-h-full max-w-full bg-slate-300 text-slate-900 font-serif px-12 py-12"
+  >
     <div class="max-w-4xl mx-auto flex flex-col items-center">
-        <h1 class="text-3xl sm:text-4xl leading-tight m-3">{{ "Relevant Courseworks" }}</h1>
-        <div class="w-5/6 h-3.5 dummy "></div>
+      <h1 class="text-3xl sm:text-4xl leading-tight m-3">
+        {{ "Relevant Courseworks" }}
+      </h1>
+      <div class="w-5/6 dummy">
+        <Crousel :slides="slides" />
+      </div>
     </div>
   </div>
 </template>
+<script setup>
+const slides = ref([
+  { url: "../AlgoToolbox.jpeg" },
+  { url: "../Meta-FE.jpeg" },
+  { url: "../adv-react.jpeg" },
+  { url: "../PS_I.png" },
+]);
+</script>
 <style>
 .dummy {
-    border: 2px solid black;
+  border: 2px solid black;
 }
 </style>
