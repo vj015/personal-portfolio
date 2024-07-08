88eeebb20605c5371af31ddbc8b5caec221ae122
Vipul Jain <vjain2376@gmail.com>
2024-07-09 01:21:52 +0530

    updated contact form

--- components/MessageForm.vue
+++ components/MessageForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
+  <div class="card h-fit max-w-6xl p-5 md:p-12" id="form" v-if="showForm">
     <form @submit.prevent="submit">
       <div class="mb-6">
         <div class="mx-0 mb-1 sm:mb-4">
@@ -32,7 +32,8 @@
               class="mb-2 w-full rounded-md border border-slate-300 py-2 pl-2 pr-4 shadow-md text-slate-900 sm:mb-0"
               name="email"
               required="true"
-              pattern="[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}" title="Please enter a valid email"
+              pattern="[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}"
+              title="Please enter a valid email"
             />
           </div>
         </div>
@@ -63,11 +64,25 @@
       </div>
     </form>
   </div>
+  <div class="flex justify-center my-24" v-if="!showForm">
+    <h1 class="m-4 text-6xl formathead">{{ "Thankyou for texting!" }}</h1>
+  </div>
 </template>
 <script setup>
+const showForm = ref(true);
 const submit = (e) => {
   e.preventDefault();
   console.log(e);
   console.log("Inside submit function");
+  showForm.value = false;
 };
 </script>
+<style scoped>
+.formathead {
+  font-family: "Sacramento", cursive;
+  text-transform: none;
+  font-weight: 400;
+  color: #c1cad7 !important;
+  font-size: 3em !important;
+}
+</style>
