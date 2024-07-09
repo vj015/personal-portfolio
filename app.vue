2cd5085b45f03059e15ade9a933dd32890136de0
Vipul Jain <vjain2376@gmail.com>
2024-07-09 20:33:09 +0530

    added meta tags

--- app.vue
+++ app.vue
@@ -14,4 +14,14 @@ useHead({
   meta: [],
   bodyAttrs: {},
 });
+useSeoMeta({
+  title: "Vipul Jain",
+  ogTitle: "Vipul Jain - Full Stack Developer",
+  description:
+    "Welcome to my portfolio! I'm Vipul Jain, a design-oriented full-stack software engineer specializing in Payments. With a passion for crafting elegant interfaces and scalable solutions, I bring creativity and technical expertise to every project. Explore my work and discover how I blend technology with design to create exceptional user experiences.",
+  ogDescription:
+    "Explore the work of Vipul Jain, a design-focused full-stack software engineer specializing in Payments. Discover elegant interfaces and scalable solutions that blend technology and design seamlessly.",
+  ogImage: "https://vipuljain.in/pikachu.jpg",
+  ogUrl: "https://vipuljain.in",
+});
 </script>
