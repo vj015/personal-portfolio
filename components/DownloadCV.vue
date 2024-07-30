769c34a7b8445ace14d8e4b7314a1e00a454b580
Vipul Jain <vjain2376@gmail.com>
2024-07-30 07:46:52 +0530

    updated cv

--- components/DownloadCV.vue
+++ components/DownloadCV.vue
@@ -5,7 +5,7 @@
     >
       {{ "Download CV" }}
     </p>
-    <a href="/resume.pdf" download="Vipul_CV">
+    <a href="/resume1.pdf" download="VipulJain_CV">
       <img src="/public/dwnld.webp" @click="downloadResume()" id="cv" />
     </a>
   </div>
